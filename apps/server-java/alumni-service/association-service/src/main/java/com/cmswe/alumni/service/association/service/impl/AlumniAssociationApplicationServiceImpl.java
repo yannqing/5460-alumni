9bcacd1a7a63aa6a7c22c9e79b56f84f32307d07
18863657292 <2318459162@qq.com>
2026-03-05 10:19:25 +0800

    fix

--- apps/server-java/alumni-service/association-service/src/main/java/com/cmswe/alumni/service/association/service/impl/AlumniAssociationApplicationServiceImpl.java
+++ apps/server-java/alumni-service/association-service/src/main/java/com/cmswe/alumni/service/association/service/impl/AlumniAssociationApplicationServiceImpl.java
@@ -28,6 +28,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Resource;
+import java.util.HashMap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
